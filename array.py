e2625138e678c622b9516e500093a869fe2c7482
kauamsantos100-hash <kauamsantos100@gmail.com>
2025-09-01 19:53:35 -0300

    Add files via upload

---
+++ array.py
@@ -0,0 +1,9 @@
+alunos = ["Kauãm", "Felipe", "Bobmarley", "Italo", "L7"] 
+print(alunos[0])  
+print(alunos[2])
+print(alunos[-1])
+
+if "Carla" in alunos:
+    print("Carla está na lista!")
+else:
+    print("Carla não foi encontrada na lista.") 
